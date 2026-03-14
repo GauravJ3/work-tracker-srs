c33aa9f08d10c0c73d06a1f3809556ed42de08bf
Gaurav Joshi <gauravjoshi12379@gmail.com>
2026-03-14 16:05:23 +0530

    fix: set correct Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? "/work-tracker-srs/" : "/",
-});
+  base: command === "build" ? "/work-tracker-srs/" : "/",
+}));
